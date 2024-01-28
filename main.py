fa0c5294b2df0702698f98113a8cc919219f2215
Vittorio Lo Mele <vittorio@mrbackslash.it>
2024-01-28 17:25:19 +0100

    test template

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/public", StaticFiles(directory="public"), name="public")
+app.mount("/lib", StaticFiles(directory="node_modules"), name="node_modules")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+def homepage(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="homepage.html",
+        context={"current_year": "2023"}
+    )
